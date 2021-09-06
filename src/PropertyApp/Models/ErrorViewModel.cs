8edab0fa8899770c291a531c82254a4f85438fd3
Christianah Omotoyinbo <CMotoy@outlook.com>
2021-09-06 14:35:04 +0100

    Added Data Project

---
+++ src/PropertyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PropertyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
