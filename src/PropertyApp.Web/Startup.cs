6fe3dfdde032b52c0d091499d6505639eaf40460
Christianah Omotoyinbo <CMotoy@outlook.com>
2021-09-06 16:01:37 +0100

    added database to startup.cs

--- src/PropertyApp.Web/Startup.cs
+++ src/PropertyApp.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PropertyApp.Data.DatabaseContext.AuthenticationDbContexts;
 
 namespace PropertyApp.Web
 {
@@ -15,6 +17,7 @@ namespace PropertyApp.Web
     {
         public Startup(IConfiguration configuration)
         {
+            
             Configuration = configuration;
         }
 
@@ -23,6 +26,8 @@ namespace PropertyApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
